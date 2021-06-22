5d386c778c4d9f2392cc2e358bb3416e075a5208
Kyle Davis <kyledavis@Kyles-iMac.local>
2021-06-21 18:12:30 -0600

    header added

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+// import Firework from '../gif/firework.gif'
+
+export default function Header() {
+  return (
+    <div className="header-container">
+      <h1 className="header-text"><span className="first-annual">The First annual, </span><span className="bluff-street-blowout">"Bluff Street Blowout"</span><br></br>GARAGE<br></br>SALE!<br></br><span>sdgf</span></h1>
+      <div>
+        <p className="header-paragraph">From skis to dehydrators and everything else in between, we have it all!</p>
+      </div>
+    </div>
+  )
+}
