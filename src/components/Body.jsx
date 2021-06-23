ed25d5207cfe7b8f8dab311b016a3687e6665117
Kyle Davis <kyledavis@Kyles-iMac.local>
2021-06-23 14:59:40 -0600

    body section

---
+++ src/components/Body.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Body.css'
+
+export default function Body() {
+  return (
+    <div className="body-container">
+      <h1>SATURDAY,<br></br>JULY 10TH —<br></br><span className="event-time">10 AM to 5 PM</span></h1>
+    </div>
+  )
+}
